6d516576bb741b7cc4b6618dc4c82e8b9da6b9f3
Han <hanshengzhao1993@gmail.com>
2018-04-28 16:10:15 -0700

    added cancel button

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,24 +1,34 @@
+import { Link } from 'react-router-dom';
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input
           className="form-control"
           type="text"
           {...field.input}
         />
-        {field.meta.error}
+        <div className="text-help">
+          { touched ? error : ''}
+        </div>
       </div>
     )
   }
 
+  onSubmit(values) {
+    console.log(values)
+  }
+
   render() {
+    const { handleSubmit } = this.props;
     return (
-      <form onSubmit={}>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           label="Title"
           name="title"
@@ -41,6 +51,10 @@ class PostsNew extends Component {
           type="submit"
           className="btn btn-primary"
         >Submit</button>
+
+        <Link className="btn btn-danger" to="/">
+          Cancel
+        </Link>
       </form>
     )
   }
