797db034482077af26d507adc3109810fd951cbf
Han <hanshengzhao1993@gmail.com>
2018-04-27 19:51:38 -0700

    added new post page

---
+++ src/components/post_new.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+// import { fetchPosts } from '../actions';
+// import _ from 'lodash';
+
+export default class PostsIndex extends Component {
+  render() {
+    return (
+      <div>
+        NEW POSTS TIME
+      </div>
+    )
+  }
+}
+
+// function mapStateToProps(state) {
+//   return { posts: state.posts };
+// }
+
+// export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
