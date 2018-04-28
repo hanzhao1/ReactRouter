b87a32a8b881540fd9571b97f1ef2357e9edc0ef
Han <hanshengzhao1993@gmail.com>
2018-04-28 15:28:20 -0700

    added redux form

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 // import { connect } from 'react-redux';
 // import { fetchPosts } from '../actions';
 // import _ from 'lodash';
 
-export default class PostsIndex extends Component {
-  render() {
+class PostsNew extends Component {
+  renderTitleField(field) {
     return (
       <div>
-        NEW POSTS TIME
+        <input
+          type="text"
+          {...field.input}
+        />
       </div>
     )
   }
+
+  render() {
+    return (
+      <form>
+        <Field
+          name="title"
+          component={this.renderTitleField}
+        />
+      </form>
+    )
+  }
 }
+
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew)
 
 // function mapStateToProps(state) {
 //   return { posts: state.posts };
