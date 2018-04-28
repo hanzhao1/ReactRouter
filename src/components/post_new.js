0652672b9dbcb5a4124ede4d86cb33982c1ed710
Han <hanshengzhao1993@gmail.com>
2018-04-28 15:49:54 -0700

    added validation

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,39 +1,70 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
-// import { connect } from 'react-redux';
-// import { fetchPosts } from '../actions';
-// import _ from 'lodash';
 
 class PostsNew extends Component {
-  renderTitleField(field) {
+  renderField(field) {
     return (
-      <div>
+      <div className="form-group">
+        <label>{field.label}</label>
         <input
+          className="form-control"
           type="text"
           {...field.input}
         />
+        {field.meta.error}
       </div>
     )
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={}>
         <Field
+          label="Title"
           name="title"
-          component={this.renderTitleField}
+          component={this.renderField}
         />
+
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+
+        <button
+          type="submit"
+          className="btn btn-primary"
+        >Submit</button>
       </form>
     )
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if(!values.title){
+    errors.title = "Enter a title!";
+  }
+
+  if(!values.categories){
+    errors.categories = "Enter a categories!";
+  }
+
+  if(!values.content){
+    errors.content = "Enter some content!";
+  }
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew)
-
-// function mapStateToProps(state) {
-//   return { posts: state.posts };
-// }
-
-// export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
